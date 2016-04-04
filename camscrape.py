25dc0d8eb1db8f7905239e374dbe5a087df34fc1
quillaja <ben@dh>
2016-04-04 04:34:14 -0700

    added parts to make scraper function only between astronomical sunrise and sunset, using PyEphem.

--- camscrape.py
+++ camscrape.py
@@ -2,6 +2,7 @@
 import sys
 import time
 
+import ephem
 import requests
 
 def do(func, times, end=None, pause=0):
@@ -24,6 +25,23 @@ def do(func, times, end=None, pause=0):
 
     return None, i
 
+def sun():
+    """Use PyEphem to calculate sunrise and sunset for the current day."""
+    portland = ephem.Observer()
+    portland.horizon = '-18' #-6, -12, -18: civil, nautical, astronomical
+    portland.elevation = 4000 #4000 meters (ie Hood summit)
+    portland.lat, portland.lon = '45.5', '-122.6' #Portland, OR 45.4923221,-122.6394305
+    portland.date = time.strftime('%Y/%m/%d') + ' 12:00:00' #today at noon
+
+    srise = portland.previous_rising(ephem.Sun(), use_center=True)
+    sset = portland.next_setting(ephem.Sun(), use_center=True)
+    return (ephem.localtime(srise).hour, ephem.localtime(sset).hour + 1)
+
+def is_between_twilight():
+    """Test if current hour is between sunrise and sunset."""
+    srise, sset = sun()
+    return srise <= time.localtime().tm_hour <= sset
+    
 def scrape():
     """
     Does the scraping.
@@ -32,7 +50,7 @@ def scrape():
     tformat = time.strftime('%Y-%m-%d %H:%M:%S')
     url = 'http://www.timberlinelodge.com/wp-content/themes/Jupiter-child/cams/palmerbottom.jpg?nocache=' + t
     filename = '/home/quillaja/static.quillaja.net/palmer/img/palmer_%s.jpg' % t
-    logfile = 'scrape.log'
+    logfile = '/home/quillaja/static.quillaja.net/palmer/scrape.log'
     
     # attempt to get url up to 2 times, with 60 seconds between each attempt
     r, tries = do(lambda: requests.get(url), 2, lambda r: len(r.content) > 30000, 60)
@@ -45,9 +63,9 @@ def scrape():
     else:
         status = '%s: Failed after %s tries.\n' % (tformat, tries)
         
-    with open(logfile, 'w') as log:
-        log.write(status)
-
-# do actual scraping only bewteen 5am and 10pm, not at night
-if True:#5 <= time.localtime().tm_hour <= 22:
+    with open(logfile, 'a') as f:
+        f.write(status)
+    
+# do actual scraping only bewteen astronomical twilight sunrise and sunset, not at night
+if is_between_twilight():
     scrape()
--- zipday.sh
+++ zipday.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-
 zip -9 /home/quillaja/static.quillaja.net/palmer/img/archive/$(date +%F).zip /home/quillaja/static.quillaja.net/palmer/img/*.jpg

7cd1af57011ce504d7e624049c036f8e319f0248
quillaja <quillaja@hotmail.com>
2016-04-29 18:41:03 -0700

    changed MIN_VALID_IMG_SIZE in settings.py from 30k to 25k. Been loosing a lot of images during storms/white-out.

--- settings.py
+++ settings.py
@@ -5,7 +5,7 @@ ACTIVELOG_DATEFMT = '%Y-%d-%m %H:%M:%S'
 TWILIGHT_TYPE = 'nautical'
 WEBCAM_URL = 'http://www.timberlinelodge.com/wp-content/themes/Jupiter-child/cams/palmerbottom.jpg?nocache={}'
 IMAGE_PATH = '/home/quillaja/static.quillaja.net/palmer/img/palmer_{}.jpg'
-MIN_VALID_IMG_SIZE = 30000
+MIN_VALID_IMG_SIZE = 25000
 
 LOC_ELEV = 3500  #3500 meters (ie Hood summit)
 LOC_LAT = '45.37'  #Hood summit 45.373505,-121.6962728

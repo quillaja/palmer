50dfc7dfc4806e70786a11ef85929bda15ffd4dd
ben <quillaja@hotmail.com>
2016-06-06 10:05:39 -0700

    tweak to zipday.sh to show the log summary before the zip output.

--- zipday.sh
+++ zipday.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-#make zip archive
+#make copy of day's log for use
 cp /home/quillaja/static.quillaja.net/palmer/scrape.log /home/quillaja/static.quillaja.net/palmer/img/archive/$(date +%F).log
+#send day's log stats to email
+python /home/quillaja/static.quillaja.net/palmer/logstats.py $(date +%F)
+#make zip archive
 zip -9 -m -j /home/quillaja/static.quillaja.net/palmer/img/archive/$(date +%F).zip /home/quillaja/static.quillaja.net/palmer/img/*.jpg /home/quillaja/static.quillaja.net/palmer/img/archive/$(date +%F).log
+#store a copy of the day's log, unzipped, in the archive
 mv /home/quillaja/static.quillaja.net/palmer/scrape.log /home/quillaja/static.quillaja.net/palmer/img/archive/$(date +%F).log
 #restore empty log
 touch /home/quillaja/static.quillaja.net/palmer/scrape.log
-#send day's log stats to email
-python /home/quillaja/static.quillaja.net/palmer/logstats.py $(date +%F)

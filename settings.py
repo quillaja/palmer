19f1a844398d2518b9d87c135c126ace84b0b301
ben <quillaja@hotmail.com>
2017-09-30 15:49:29 -0700

    added more detail to USER_AGENT

--- settings.py
+++ settings.py
@@ -15,7 +15,7 @@ LOC_ELEV = 3500  #3500 meters (ie Hood summit)
 LOC_LAT = '45.37'  #Hood summit 45.373505,-121.6962728
 LOC_LON = '-121.70'
 
-USER_AGENT = 'Mozilla/5.0'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
 
 # log stats
 ARCHIVE_PATH = '/home/quillaja/static.quillaja.net/palmer/img/archive/'
